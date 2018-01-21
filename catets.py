866f589750927690a501addc685c234b9fdf309a
0mUltramar <korban.s.a@gmail.com>
2018-01-22 02:22:42 +0300

    Sasha's Tasks

---
+++ catets.py
@@ -0,0 +1,7 @@
+hiMessage = ("Без регистрации и смс получи катет прям. треуг. просто введя катеты a и b.")
+print(hiMessage)
+count = 0
+a = input('катет a: ')
+b = input('катет b: ')
+gipotenuza = math.sqrt(a ** 2 + b ** 2)
+print("Гиотенуза: %.2f" % gipotenuza)
---
+++ task2.py
@@ -0,0 +1,5 @@
+print('Write two numbers (a and b), which you wanna multiplicate')
+a = int(input("a: "))
+b = int(input("b: "))
+c = a + b
+print('a + b =', c)
