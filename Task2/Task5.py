513d2663ee0ccb6801ff87b1ed0ba9a27828e060
omultramar <korban.s.a@gmail.com>
2018-01-22 18:04:35 +0300

    optimize files repository

---
+++ Task2/Task5.py
@@ -0,0 +1,9 @@
+
+# Выведу количество часов и минут без палева и таможенных сборов
+
+setMinutes = int(input("Введите количество минут: "))
+showHours = setMinutes // 60 % 24 # // - целочисл. деление, возвращаем только целую часть деления
+showMinutes = setMinutes % 60 # % - возвращает остаток
+# подробнее можно узнать на http://pythonicway.com/python-operators
+
+print("На часах: {}:{}".format(showHours, showMinutes))
