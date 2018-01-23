2c8d90305ab7278e6489e2d693626c8e13d21f4f
0mUltramar <korban.s.a@gmail.com>
2018-01-23 18:48:56 +0300

    test5 improvements

--- Task3/Task2p2.py
+++
@@ -1,9 +0,0 @@
-X = float(input("Введите число, соотвествующее оси Х: "))
-Y = float(input("Введите число, соотвествующее оси Y: "))
-if X > 0 and Y > 0:
-    print('I')
-    elif: X > 0
-        print('IV')
-    elif: Y > 0:
-        print('II')
-else: print('III')
