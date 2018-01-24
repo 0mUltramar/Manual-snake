f494ba1e4cae61a37921a64b0fb4206a15366c5f
0mUltramar <korban.s.a@gmail.com>
2018-01-24 11:53:16 +0300

    Задание 4

--- Task3/Task4.py
+++ Task3/Task4.py
@@ -4,13 +4,17 @@
 # y = -x, при x < 10.
 
 print("Программа вычисляет Y из функции y = f(x) по введенному значению X")
+
 X = float(input('X = '))
+
 if X > 10:
     Y = X + 1.5
     print("Y = {}".format(Y))
+
 elif X == 10:
     Y = 0
     print("Y = {}".format(Y))
+
 elif X < 10:
     Y = X * (-1)
     print("Y = {}".format(Y))
