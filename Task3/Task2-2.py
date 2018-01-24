f1ab6ee1f6781c489bcb1133906f0b49627dcb62
0mUltramar <korban.s.a@gmail.com>
2018-01-24 11:51:02 +0300

    Задание 2

--- Task3/Task2-2.py
+++ Task3/Task2-2.py
@@ -1,20 +1,29 @@
-# Привет)
+# Настоящее задание №2 находится тут. Спасибо, что делаешь код ревью)
+
 print('Программа вычислит четверть, в которой лежит точка на графике')
 X = float(input("Введите число, соотвествующее оси Х: "))
 Y = float(input("Введите число, соотвествующее оси Y: "))
+
 if X == 0:
+
     if Y != 0:
         print ('OX')
+
     elif Y == 0:
         print('BINGO! Black ZERO!')
+
 elif Y == 0:
     if X != 0:
         print('OY')
+
 elif X > 0 and Y > 0:
     print('I')
+
 elif X < 0 and Y < 0:
     print('III')
+
 elif X < 0 and Y > 0:
     print('II')
+
 else:
     print('IV')
