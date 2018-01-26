84c2c722d0dba89607264adf4095abf3c5e874e4
0mUltramar <korban.s.a@gmail.com>
2018-01-26 11:23:42 +0300

    Задание 1

--- Task4/Task1.py
+++ Task4/Task1.py
@@ -1,4 +1,4 @@
-print("выведу числа от 1 до 5 тремя способами")
+print("выведу числа от 1 до 5 двумя способами")
 a = 1
 print("способ 1")
 while 0 < a <= 5:
@@ -8,13 +8,6 @@ print()
 print()
 
 print("способ 2")
-for i in [1, 2, 3, 4, 5]:
-    print(i, '', end='')
-print()
-print()
-
-print("способ 3")
-
 for i in range(1, 6):
     print(i, '', end='')
 print()
