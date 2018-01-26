286654c2c7d8d195fbfd431c7c5fe6d22f2d165b
0mUltramar <korban.s.a@gmail.com>
2018-01-26 22:15:06 +0300

    Задание 3 in progress

--- Task4/Task3.py
+++ Task4/Task3.py
@@ -1,13 +1,21 @@
+x = (input("Введите положительное целое число:"))
 try:
-    x = (input(':'))
-    if x != abs(x):
+    x = int(x)
+    if int(x) != abs(x):
         print("Вы ввели отрицательное число. Не накаляй, введи положительное!")
     else:
         if float(x) != int(x):
             print("Ты заставляешь меня снова на тебя орать. ВВЕДИ ЦЕЛОЕ ЧИСЛО!")
         else:
-            print(x + 1)
+            #формируем список
+            rangeList = []
+            theSum = 0
+            for i in range(1, x):
+
+
+                print("Сумма чисел от 1 до {0} = {1}".format(x, sum(rangeList)))
 except ValueError:
-    print("Какая то хуйня")
+    print("Какая-то херня, браток")
 except Exception:
     print("Даже я не оиждал такой ереси")
+# язык Python за десять минут
