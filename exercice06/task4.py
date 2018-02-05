e40189398788aa3e9598f130b10ae3767441de3b
omultramar <korban.s.a@gmail.com>
2018-02-05 23:56:52 +0300

    Задание 4

--- exercice06/task4.py
+++ exercice06/task4.py
@@ -8,17 +8,25 @@ listLim = 6
 
 # rnd.randint(A, B) - генерит случайное целое число N, где A ≤ N ≤ B, упаковываем в квадр. скобки,
 # тем самым заюзаем конструктор списков
-i = [rnd.randint(1, 100) for _ in range(listLim)]
-print("Сгенерили список: {}".format(i))
-print("Самое большое число в списке: {}".format(max(i)))  # значение функции max прозрачно жеж)
-
+iList = [rnd.randint(1, 100) for _ in range(listLim)]
+print("Сгенерили список: {}".format(iList))
+print("Самое большое число в списке: {}".format(max(iList)))  # значение функции max прозрачно жеж)
 
 # второй способ
+maxValue = 0
+for i in iList:
+    if i > maxValue:
+        maxValue = i
+print('Самое большое число в списке: {}'.format(maxValue))
+
+# третий способ
+
 print("~" * 50)
+
 revList = []
-for i in range(7):
+for i in range(listLim):
     a = rnd.randint(100, 1000)
     revList.append(a)
 print("Список в естественной последовательности: {}".format(revList))
 revList.sort(reverse=True)
-print("наибольшее значние в списке: {} ".format(revList.pop(0)))
+print("Самое большое число в списке: {} ".format(revList.pop(0)))
