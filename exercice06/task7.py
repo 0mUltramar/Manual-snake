fe43975358d842186f860cef3cd6485599ab4806
omultramar <korban.s.a@gmail.com>
2018-02-09 01:12:06 +0300

    Задача 9 in progress

--- exercice06/task7.py
+++ exercice06/task7.py
@@ -2,7 +2,11 @@
 # Удалить те элементы, значения которых не кратны 3.
 
 import random as rnd
-tenList = [rnd.randint(1, 1000) for i in range(10)]
+
+limitList = 10
+
+tenList = [rnd.randint(1, 1000) for i in range(limitList)]
 iList = [x for x in tenList if x % 3 == 0]
+
 print("Исходный список из 10 значений: {}".format(tenList))
 print("Список из значений, кратных трем: {}".format(iList))
